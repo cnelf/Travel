3b72e1f31d62caebe271666d24ba592c53d039a2
CNelfHope <1184974829@qq.com>
2018-11-19 18:07:55 +0800

    add city-list

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper">
+    <ul class="list">
+      <li class="ltem">A</li>
+      <li class="ltem">B</li>
+      <li class="ltem">C</li>
+      <li class="ltem">D</li>
+      <li class="ltem">E</li>
+      <li class="ltem">F</li>
+      <li class="ltem">G</li>
+      <li class="ltem">H</li>
+      <li class="ltem">I</li>
+      <li class="ltem">J</li>
+      <li class="ltem">K</li>
+      <li class="ltem">L</li>
+      <li class="ltem">M</li>
+      <li class="ltem">N</li>
+      <li class="ltem">O</li>
+      <li class="ltem">P</li>
+      <li class="ltem">Q</li>
+      <li class="ltem">R</li>
+      <li class="ltem">S</li>
+      <li class="ltem">T</li>
+      <li class="ltem">U</li>
+      <li class="ltem">V</li>
+      <li class="ltem">W</li>
+      <li class="ltem">X</li>
+      <li class="ltem">Y</li>
+      <li class="ltem">Z</li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import '~styles/variables.styl'
+  .list
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    display: flex
+    flex-direction: column
+    justify-content: center
+    width: .4rem
+    .ltem
+      line-height: .4rem
+      text-align: center
+      color: $bgColor
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="wrapper">
+    <div class="list" ref="list">
+      <div>
+        <div class="city-area">
+          <h1 class="city-title">当前城市</h1>
+          <ul class="city-list">
+            <li class="city-wrapper">
+              <div class="city">北京</div>
+            </li>
+          </ul>
+        </div>
+        <div class="city-area">
+          <h1 class="city-title">热门城市</h1>
+          <ul class="city-list">
+            <li class="city-wrapper">
+              <div class="city">北京</div>
+            </li>
+            <li class="city-wrapper">
+              <div class="city">北京</div>
+            </li>
+            <li class="city-wrapper">
+              <div class="city">北京</div>
+            </li>
+            <li class="city-wrapper">
+              <div class="city">北京</div>
+            </li>
+          </ul>
+        </div>
+        <div class="city-area">
+          <h1 class="city-title">A</h1>
+          <ul class="item-list">
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+          </ul>
+        </div>
+        <div class="city-area">
+          <h1 class="city-title">B</h1>
+          <ul class="item-list">
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+          </ul>
+        </div>
+        <div class="city-area">
+          <h1 class="city-title">C</h1>
+          <ul class="item-list">
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+            <li class="item border-bottom">阿坝藏族羌族自治州</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
+
+export default {
+  name: 'CityList',
+  mounted() {
+    this.scroll = new BScroll(this.$refs.list)
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import '~styles/variables.styl'
+  .border-bottom
+    &:before
+      border-color: #ccc
+  .list
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    overflow: hidden
+    .city-title
+      display: block
+      height: .74rem
+      line-height: .74rem
+      text-indent: .2rem
+      font-size: .24rem
+      background: #f5f5f5
+      color: $textColor
+    .city-list
+      overflow: hidden
+      padding: .1rem .6rem .1rem .1rem
+      .city-wrapper
+        width: 33.33%
+        float: left
+      .city
+        margin: .1rem
+        padding: .1rem 0
+        text-align: center
+        border: .02rem solid #ccc
+        border-radius: .06rem
+        color: $textColor
+    .item-list
+      line-height: .9rem
+      text-indent: .2rem
+      font-size: .28rem
+      color: $textColor
+</style>
